24fc648f04eca7946a3794a33a123654729cf66f
danswann <116047734+danswann@users.noreply.github.com>
2022-10-17 23:23:58 -0400

    Correct rule 03 example

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -3,6 +3,6 @@
    Use integer types that can fully represent the range of unsigned data
 */
 
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
